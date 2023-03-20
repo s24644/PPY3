021a2e86e2b8d0be175aec06d84badc4fc7a92de
s24644 <s24644@pjwstk.edu.pl>
2023-03-20 15:19:44 +0100

    zadanie 3 (tylko gra z komputerem)

--- PPY3/main.py
+++ PPY3/main.py
@@ -35,3 +35,42 @@ for i in range(0,len(moja_lista)):
     print(moja_lista[random_index])
     moja_lista.pop(random_index)
     counter = counter - 1
+
+#=-------------Zadanie 3-----------------------------
+print("=-=-=-=-=Zadanie 3=-=-=-=-=-=-")
+
+rounds = input("podaj liczbe rund")
+rounds = int(rounds)
+
+game_type = input("podaj tryb gry (komputer/2 graczy)")
+
+options_list = ["papier", "kamien", "nozyce"]
+
+if(game_type == "komputer"):
+    for i in range(0, rounds):
+        player_weapon = input("Podaj swoja broń: ")
+        randomVal = random.randint(0,2)
+        computer_weapon = options_list[randomVal]
+        print("Bron przeciwnika: ", computer_weapon)
+
+        if player_weapon == "papier" and computer_weapon == "papier":
+            print("remis")
+        elif player_weapon == "papier" and computer_weapon == "kamien":
+            print("Wygrywasz, gratulacje")
+        elif player_weapon == "papier" and computer_weapon == "nozyce":
+            print("Niestety przegrywasz")
+        elif player_weapon == "kamien" and computer_weapon == "papier":
+            print("Niestety przegrywasz")
+        elif player_weapon == "kamien" and computer_weapon == "kamien":
+            print("remis")
+        elif player_weapon == "kamien" and computer_weapon == "nozyce":
+            print("Wygrywasz, gratulacje")
+        elif player_weapon == "nozyce" and computer_weapon == "papier":
+            print("Wygrywasz, gratulacje")
+        elif player_weapon == "nozyce" and computer_weapon == "kamien":
+            print("Niestety przegrywasz")
+        elif player_weapon == "nozyce" and computer_weapon == "nozyce":
+            print("remis")
+
+
+
