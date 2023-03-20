e6736dec43f593420ed6dbc451a8c35ab9e33a0a
s24644 <s24644@pjwstk.edu.pl>
2023-03-20 14:53:49 +0100

    zadanie 2

--- PPY3/main.py
+++ PPY3/main.py
@@ -1,7 +1,8 @@
 import sys
-
+import random
 
 #-------------zadanie 1-------------------------------
+print("=-=-=-=-Zadanie 1=-=-=-=-=")
 
 liczby = input("podaj liczby oddzielone przecinkiem")
 
@@ -17,9 +18,20 @@ for i in liczby:
     if integerVal < min:
         min = integerVal
 
-print(max)
-print(min)
+print("Max:", max)
+print("Min: ", min)
 
 #--------------zadanie 2---------------------------
+print("-=-=-=-Zadanie 2-=-=-=-=")
 
+moja_lista = ["Warszawa", "Kraków", "Wrocław", "Łódź",
+              "Poznań","Gdańsk", "Szczecin",
+              "Bydgoszcz", "Lublin", "Białystok"]
 
+counter = len(moja_lista)-1
+
+for i in range(0,len(moja_lista)):
+    random_index = random.randint(0,counter)
+    print(moja_lista[random_index])
+    moja_lista.pop(random_index)
+    counter = counter - 1
